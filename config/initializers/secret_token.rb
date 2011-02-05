6a100d9e39a35904faae00f9fbbe8603cfa9d4a2
Erik DeBill <erik@debill.org>
2011-02-05 14:11:31 -0600

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megstv::Application.config.secret_token = 'ea6ded8acaf3e7919572653997b34539c8c78e4b7555b2a6d7d8ddba291dad1ec07bb6d2e204f696e5121d1878d5a484437e8a8e4f1f7292eea9c8ae92ecf58f'
