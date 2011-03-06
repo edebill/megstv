1529aaee1753e128ebb3c8fef458f909471fadb8
Erik DeBill <erik@debill.org>
2011-03-06 11:07:39 -0600

    remove sessions table from db

--- db/schema.rb
+++ db/schema.rb
@@ -28,16 +28,6 @@ ActiveRecord::Schema.define(:version => 20110306164733) do
 
   add_index "minutes", ["user_id"], :name => "index_minutes_on_user_id"
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "",   :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "",   :null => false
