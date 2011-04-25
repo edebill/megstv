182ddaa2a58a81d6a977360d7f03bb318210bfe6
Erik DeBill <edebill@moxiesoft.com>
2011-04-25 18:40:54 -0500

    send email via gmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,17 @@ Megstv::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'megstv.debill.org' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => "debill.org",
+    :username => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true }
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
